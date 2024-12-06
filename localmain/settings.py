39cb8fc1648e32747bff01da35b3a9995e819ad4
SurF-Fit <kkulmenev@gmail.com>
2024-12-06 11:09:06 +0700

    create category lists and detail. Maybe complete project:))

--- localmain/settings.py
+++ localmain/settings.py
@@ -94,9 +94,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    'C:/Users/kulmenev/djangoProject/main/static',
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
