960fa1e39a665c4ef5e880d8a1c729553c03efe8
SurF-Fit <kkulmenev@gmail.com>
2024-12-07 08:41:29 +0700

    create slider

--- main/static/js/slider.js
+++ main/static/js/slider.js
@@ -1,23 +1,23 @@
 const slider = document.querySelector('.slider');
-    const prevButton = document.querySelector('.prev-button');
-    const nextButton = document.querySelector('.next-button');
-    const slides = Array.from(document.querySelectorAll('.slide'));
-    let slideIndex = 0;
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(document.querySelectorAll('.slide'));
+let slideIndex = 0;
 
-    const updateSlider = () => {
-        const imageWidth = slider.clientWidth;
-        const slideOffset = -slideIndex * imageWidth / 4;
-        slider.style.transform = `translateX(${slideOffset}px)`;
-    };
+const updateSlider = () => {
+    const imageWidth = slider.clientWidth;
+    const slideOffset = -slideIndex * imageWidth / 4;
+    slider.style.transform = `translateX(${slideOffset}px)`;
+};
 
-    prevButton.addEventListener('click', () => {
-        slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-        updateSlider();
-    });
+prevButton.addEventListener('click', () => {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    updateSlider();
+});
 
-    nextButton.addEventListener('click', () => {
-        slideIndex = (slideIndex + 1) % slides.length;
-        updateSlider();
-    });
+nextButton.addEventListener('click', () => {
+    slideIndex = (slideIndex + 1) % slides.length;
+    updateSlider();
+});
 
-    window.addEventListener('load', updateSlider);
+window.addEventListener('load', updateSlider);
